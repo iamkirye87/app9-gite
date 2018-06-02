17eda7f0dfbbf74098a2d4e5e28e151a1d19e411
lopezmartineznelson16 <38304993+lopezmartineznelson16@users.noreply.github.com>
2018-06-02 14:13:53 -0500

    subida8

--- js/acciones.js
+++ js/acciones.js
@@ -1,43 +1,42 @@
 // JavaScript Document
-
-$(document).ready(function(e) {
-document.addEventListener("deviceready", onDeviceReady, false);
+$(document).ready(function (e) {
+	document.addEventListener("deviceready",onDeviceReady,false);
 
 });
 
 function onDeviceReady(){
 	
-$('posicion').on('click',function(){
+$('#posicion').on('click',function(){
 	getPosition();
 	});
 	
 $('#watch').on('click',function(){
 	watchPosition();
-});
-
+	});
+	
 }
 
-	function getPosition(){
+function getPosition(){
 	var options={
-		enableHighAccuaracy:true,
+		enableHighAccuracy:true,
 		maximumAge:3600000
 	}
 	
-	var watchID=navigator.geolocation.getCurrentPosition(onSuccess. onError, options);
-	function onSurccess(position){
+	var watchID=navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+	function onSuccess(position){
 		
-		alert('latitude:' +position.coords.latitude +'\n'+
-		'longitude:' +position.coords.longitude +'\n'+
-		'altitude:' +position.coords.altitude +'\n'+
-		'accuarcy:' +position.coords.accuarcy +'\n'+
-		'altitude Accuarcy:' +position.coords.altitudeAccuarcy +'\n'+
-		'heading:' +position.coords.heading +'\n'+
-		'speed:' +position.coords.speed +'\n'+
-		'timestamp:' +position.coords.timestamp +'\n');
-	};
+		alert('latitude:'	+ position.coords.latitude	+ '\n' +
+		'Longitude:'	+ position.coords.longitude		+ '\n' +
+		'Altitude:'		+ position.coords.altitude		+ '\n' +
+		'Accuracy:'		+ position.coords.accuracy		+ '\n' +
+		'Altitude Accuracy:'	+ position.coords.altitudeAccuracy		+ '\n' +
+		'Heading:'		+ position.coords.heading		+ '\n' +
+		'Speed:'		+ position.coords.speed		+ '\n' +
+		'Timestamp'		+ position.timestamp	+ '\n');
+	}
 	
 	function onError(error){
-		alert('code:' +error.code +'\n'+ 'message:' + error.message +'\n');
+		alert('code:' +error.code	+ '\n' + 'message:' + error.message + '\n' );
 	}
 }
 
@@ -45,22 +44,22 @@ function watchPosition(){
 	var options={
 		maximumAge:3600000,
 		timeout:3000,
-		enableHighAccuarcy:true,
+		enableHighAccuracy:true,
 	}
 	
 	var watchID=navigator.geolocation.watchPosition(onSuccess,onError,options);
 	function onSuccess(position){
-$('#latitud').html(position.coords.latitude);
-$('#longitud').html(position.coords.longitud);
-$('#altitud').html(position.coords.altitud);
-$('#accuaracy').html(position.coords.accuaracy);
-$('#aaccuaracy').html(position.coords.altitudeAccuaracy);
-$('#headding').html(position.coords.headding);
-$('#speed').html(position.coords.speed);	
-$('#timestamp').html(position.coords.timestamp);
-};
-
+		$('#latitud').html(position.coords.latitude);
+		$('#longitud').html(position.coords.longitude);
+		$('#altitud').html(position.coords.altitude);
+		$('#accuracy').html(position.coords.accuracy);
+		$('#aaccuracy').html(position.coords.altitudeAccuracy);
+		$('#headingg').html(position.coords.heading);
+		$('#speed').html(position.coords.speed);
+		$('#timestamp').html(position.timestamp);
+	}
+	
 	function onError(error){
-		alert('code:' +error.code +'\n'+ 'message:' + error.message +'\n');
+		alert('code:' +error.code	+ '\n' + 'message:' + error.message + '\n' );
 	}
 }
